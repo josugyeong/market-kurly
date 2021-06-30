74f056dd82b4bc9755f0bc5ecf8010e4f7b562e9
sugyeong jo <ssuya1020@gmail.com>
2021-06-30 17:32:08 +0900

    menu_sub add

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 'use strict';
 
-$(document).ready(function(){
-    $('.main__banner').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-
-    // custom slider
-    $('.custom__slider').slick({
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        fade: true,
-        cssEase: 'linear'
-    });
-});
+// 초기 상태 세팅
+$(function(){
+    var $submenu = $('.navbar__submenu');
+    var $navbarItem = $('.navbar__item');
+    $submenu.hide();
+    
+    // 마우스가 왔을때 submenu가 보여라
+    $navbarItem.mouseenter(function(){
+        /* 
+        var subMenu = $(this).children('.navbar__submenu');
+            subMenu.slideDown();
+        */
+       console.log(this);
+        $(this).children('.navbar__submenu').stop().slideDown();
+    })
+    $navbarItem.mouseleave(function(){
+        $(this).children('.navbar__submenu').stop().slideUp();
+    })
+})
